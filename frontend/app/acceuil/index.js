d22a34ce12864b15317a3a7c83c490c64c43e5df
MisterPy15 <agihchris90@gmail.com>
2024-12-20 09:56:29 +0000

    frontend modifier

---
+++ frontend/app/acceuil/index.js
@@ -0,0 +1,25 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+
+
+const app = () => {
+
+    return(
+        <View>
+
+            <View>
+                <Text>Acceuil Rafistoleur</Text>
+            </View>
+        
+        </ View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+
+})
+
+
+
+export default app;
---
+++ frontend/app/index.js
@@ -0,0 +1,14 @@
+import {Redirect} from 'expo-router'
+
+
+
+const app = () => {
+
+    return(
+        <Redirect href="/acceuil" />
+    )
+}
+
+
+
+export default app;
